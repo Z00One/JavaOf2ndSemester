67dcb2da8e1fd13464587a2af727bcfee8665c55
Z00One <102473964+Z00One@users.noreply.github.com>
2022-09-07 20:02:06 +0900

    DataType - Integer

---
+++ MyProject1/src/MyPrg1.java
@@ -0,0 +1,17 @@
+
+public class MyPrg1 {
+
+	public static void main(String[] args) {
+		// variable
+		int ValueA = 1;
+		int ValueB = 1;
+		
+		// 메모리 바이트 수가 같거나 더 넓은 범위로는 형변환이 가능하다.
+		long ValueC = ValueA + ValueB;
+		
+		// ++ 반대의 경우는 오류가 난다.
+		
+		System.out.println(ValueC);
+	}
+
+}
