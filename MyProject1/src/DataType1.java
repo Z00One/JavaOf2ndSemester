e4c6c229e2449ae6059eb4d647c11d08dc067bf2
Z00One <102473964+Z00One@users.noreply.github.com>
2022-09-14 16:38:44 +0900

    Data Type, Constant

---
+++ MyProject1/src/DataType1.java
@@ -0,0 +1,19 @@
+
+public class DataType1 {
+
+	public static void main(String[] args) {
+		char a = '1';
+		long foo = 1;
+		final int temp = 3; //temp is a constant
+		// temp is General Constant
+		// '1',3 is Literal Constant
+		// 리터럴은 이름 없는 상수 이름으로 호출이 불가
+		// 값 자체로 사용한다.
+		
+		short bar = 3;
+		
+		int fooo = 0x00000011;
+		System.out.println(fooo);
+	}
+
+}
