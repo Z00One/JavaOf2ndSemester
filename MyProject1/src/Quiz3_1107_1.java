e10d311a6bb6f40f13a3c8b6d0747fceafc482a9
Z00One <102473964+Z00One@users.noreply.github.com>
2022-11-08 20:53:43 +0900

    1108

--- MyProject1/src/Quiz3_1107_1.java
+++ MyProject1/src/Quiz3_1107_1.java
@@ -30,46 +30,28 @@ class inputInt {
 	// 멤버 변수 선언
 	int M;
 	int N;
-	String menuOfPrinting;
+	boolean isequalMN;
 	
 	// 멤버 변수에 값 담아주기
 	inputInt(int valueA, int valueB){
 		this.M = valueA;
 		this.N = valueB;
-		if (M < N) {
-			menuOfPrinting = "first";
-		}else if (M > N) {
-			menuOfPrinting = "second";
-		}else if (M == N) {
-			menuOfPrinting = "third";
-		}
+		isequalMN = M == N ? true : false ;
 	}
 	
 	// 담긴 변수들의 크기 차이에 따라 출력
 	void print() {
-		switch(menuOfPrinting) {
-		case "first" :
-			System.out.println("M : " + M + " ~ " + "N : " + N + "사이 정수 값은");
-			for (int i = M; i <= N; i++) {
-				System.out.print(i == N ? i + "\n" : i + ", ");
-			}
-			break;
-		case "second" :
-			System.out.println("M : " + M + " ~ " + "N : " + N + "사이 정수 값은");
-			for (int i = N; i <= M; i++) {
-				System.out.print(i == M ? i + "\n" : i + ", ");
-			}
-			break;
-		case "third" :
-			System.out.println("N, M 정수 값 : " + M);
-			break;
-			}
-		System.out.println("----thank you----");
+		if (isequalMN) {
+			
+		}
+		else {
+			
+		}
 	}
 	
 }
 
-public class Quiz3_1107 {
+public class Quiz3_1107_1 {
 
 	public static void main(String[] args) {
 		Gugu gugu = new Gugu();
